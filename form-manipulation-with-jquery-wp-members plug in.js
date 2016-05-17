8971631490c53c32b299a2fee67e970829c1176b
Your Nameajonesbgit config --global user.name Your <you@example.comalanjones83@gmail.com>
2016-05-17 11:14:47 -0500

    commiting file to github repository

---
+++ form-manipulation-with-jquery-wp-members plug in.js
@@ -0,0 +1,76 @@
+
+<?php
+/*
+Template Name: Registracion Page
+*/
+
+
+/**
+ * The template for the structure of Registration page.
+ *
+ * This template is similar to the page.php template that displays all pages by default.  */
+
+get_header(); ?>
+
+
+
+<?php 
+if(of_get_option('enable_parallax') == 1 && is_front_page() && get_option( 'show_on_front' ) == 'page'){
+	get_template_part('index','parallax');
+}else{
+?>
+
+      
+
+<div class="mid-content clearfix">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+
+
+      <div class="registrationpagecontainer">
+                                        
+                              <?php echo do_shortcode('
+                              <h1 class="registrationtitle">Registro de nuevo Usuario</h1>
+                              <p style="text-align: center; margin-top: 20px;">Por favor llena estos datos.</p>                                                         
+                                  [wp-members page="register"]
+                              '); ?>
+         
+<!--Javascript below to change layout of dynamic form which is all generated in php not html through the plug in files. -->
+ 
+            <script type="text/javascript">
+                        jQuery(document).ready(function($) {
+                        /*Removes the form class from the registration form and adds this new class below to target better form*/                       
+                            /*$('.div_text').addClass('text').removeClass('div_text');*/
+                            $('form').addClass('registrateelementswrap').removeClass('.form'); 
+                        }); 
+
+                                           
+                        /*creates new wrapper around class .text and .div_text */                       
+                         jQuery(document).ready(function($) {
+                                    $(".registrateelementswrap .text").each(function(index) {
+                                        $(this).next(".div_text").andSelf().wrapAll("<div class='registerseperator' />")
+                                    });
+                                });
+
+                        /* Removes the clear after the registerseperator added container div*/
+                          jQuery(document).ready(function($) {                        
+                         /*$('.registerseperator').next('clear').remove();*/
+                         /*$(".registrateelementswrap br.clearboth").remove();*/
+                         $("br").remove();
+                         });                     
+            </script>
+
+
+      </div>
+
+    </main><!-- #main -->
+  </div><!-- #primary -->
+
+        <?php get_sidebar(); ?>
+
+</div><!--Mid content clear fix -->
+
+<?php } ?>
+
+<?php get_footer(); ?>
